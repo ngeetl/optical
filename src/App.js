47e27043e6618a01f49b23ada410494a7c75348c
yeni <ngeetl@naver.com>
2024-05-09 00:39:04 +0900

    편광시표 최소

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import { React, useState } from 'react';
 
 function App() {
+  const lines = []; // 선을 저장할 배열
+  const degreesStep = 7.5; // 각도 단위
+  const boldDegrees = [0, 30, 60, 90, 120, 150, 180, 210, 240, 270, 300, 330, 360]; // 볼드 처리할 각도들
+  const [text, setText] = useState('')
+  const [block, setBlock] = useState(false) 
+
+  // 360도를 degreesStep 단위로 나누어 선 생성
+  for (let angle = 0; angle < 360; angle += degreesStep) {
+    const isBold = boldDegrees.includes(angle);
+    const className = isBold ? 'line-bold' : 'line';
+    lines.push(
+      <div
+        className={className}
+        key={angle}
+        style={{ transform: `rotate(${angle}deg)`, opacity: block && '1' }}
+        onClick={() => setText(angle)}
+      >
+        <span
+          style={{ transform: `rotate(calc(${angle}deg * -1))` }}
+        >{angle}º</span>
+      </div>
+      
+    );
+  }
+
+  const btnClick = () => {
+    setBlock(prev=>!prev);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='wrap'>
+        <h1>편광 시표</h1>
+        <div className="polarization-chart">
+          {lines}
+          <div className="center-circle">{text}º</div>
+        </div>
+        <button className='btn' onClick={btnClick}>add</button>
+      </div>
     </div>
   );
 }
